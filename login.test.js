fc55d57574a7482072fcb421f416f9930c906dba
NhatKhoa0506 <khoa.2474802010196@vanlanguni.vn>
2026-09-17 13:40:52 +0700

    login.test.js

--- login.test.js
+++ login.test.js
@@ -1,1 +1,19 @@
+const checkLogin = require('./login');
 
+describe('Kiểm thử hàm checkLogin', () => {
+    test('TC01: Đăng nhập thành công với tài khoản hợp lệ', () => {
+        expect(checkLogin('admin', '123')).toBe(true);
+    });
+
+    test('TC02: Đăng nhập thất bại với sai mật khẩu', () => {
+        expect(checkLogin('admin', 'wrongpass')).toBe(false);
+    });
+
+    test('TC03: Đăng nhập thất bại với sai tài khoản', () => {
+        expect(checkLogin('user', '123')).toBe(false);
+    });
+
+    test('TC04: Đăng nhập thất bại khi để trống thông tin', () => {
+        expect(checkLogin('', '')).toBe(false);
+    });
+});
